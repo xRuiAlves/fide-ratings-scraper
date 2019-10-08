de40a2e79b6da4f173ceb94d7c65134564c8f68e
Rui Alves <up201606746@fe.up.pt>
2019-10-08 19:05:47 +0100

    Create utils module

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
-
-const parseDate = (date) => {
-    const date_map = Object.freeze({
-        "Jan": "01",
-        "Feb": "02",
-        "Mar": "03",
-        "Apr": "04",
-        "May": "05",
-        "Jun": "06",
-        "Jul": "07",
-        "Aug": "08",
-        "Sep": "09",
-        "Oct": "10",
-        "Nov": "11",
-        "Dec": "12",
-    });
-    const month_num = date_map[date.substr(5, 3)];
-    return parseInt(`${date.substr(0, 4)}${month_num}`, 10);
-};
+const utils = require("./utils");
 
 const getElo = async (fide_num) => {
     const res = await axios.get(`http://ratings.fide.com/card.phtml?event=${fide_num}`);
@@ -47,7 +29,7 @@ const getHistory = async (fide_num, csv_output) => {
         const row = cheerio.load(table_entries[i])("td");
         history.push({
             date: row[0].children[0].data.replace(/\s/g, ""),
-            numeric_date: parseDate(row[0].children[0].data.replace(/\s/g, "")),
+            numeric_date: utils.parseDate(row[0].children[0].data.replace(/\s/g, "")),
             standard: row[1].children[0].data.replace(/\s/g, ""),
             rapid: row[3].children[0].data.replace(/\s/g, ""),
             blitz: row[5].children[0].data.replace(/\s/g, ""),
---
+++ utils.js
@@ -0,0 +1,22 @@
+const parseDate = (date) => {
+    const date_map = Object.freeze({
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12",
+    });
+    const month_num = date_map[date.substr(5, 3)];
+    return parseInt(`${date.substr(0, 4)}${month_num}`, 10);
+};
+
+module.exports = {
+    parseDate,
+};
