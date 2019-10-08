072fd936d3947f2160e7f8b54822015ac2d8901e
Rui Alves <up201606746@fe.up.pt>
2019-10-08 16:05:15 +0100

    Add elo history table scrapping

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
 const axios = require("axios");
 const cheerio = require("cheerio");
 
+const parseDate = (date) => {
+    const date_map = Object.freeze({
+        "Jan": "01",
+        "Feb": "02",
+        "Mar": "03",
+        "Apr": "04",
+        "May": "05",
+        "Jun": "06",
+        "Jul": "07",
+        "Aug": "08",
+        "Sep": "09",
+        "Oct": "10",
+        "Nov": "11",
+        "Dec": "12",
+    });
+    const month_num = date_map[date.substr(5, 3)];
+    return parseInt(`${date.substr(0, 4)}${month_num}`, 10);
+};
+
 const getElo = async (fide_num) => {
     const res = await axios.get(`http://ratings.fide.com/card.phtml?event=${fide_num}`);
     const $ = cheerio.load(res.data);
@@ -17,5 +36,28 @@ const getElo = async (fide_num) => {
     };
 };
 
+const getHistory = async (fide_num) => {
+    const res = await axios.get(`http://ratings.fide.com/id.phtml?event=${fide_num}`);
+    const $ = cheerio.load(res.data);
+    const table_entries = $(`
+        #main-col table:nth-child(2) tr:nth-child(2) td div 
+        table tr:nth-child(2) td table tbody tr`);
+    const history = [];
+    table_entries.map((i) => {
+        const row = cheerio.load(table_entries[i])("td");
+        history.push({
+            date: row[0].children[0].data.replace(/\s/g, ""),
+            numeric_date: parseDate(row[0].children[0].data.replace(/\s/g, "")),
+            standard: row[1].children[0].data.replace(/\s/g, ""),
+            rapid: row[3].children[0].data.replace(/\s/g, ""),
+            blitz: row[5].children[0].data.replace(/\s/g, ""),
+        });
+    });
+    return history;
+};
+
 getElo(process.argv[2])
     .then((elo) => console.log(elo));
+
+getHistory(process.argv[2])
+    .then((history) => console.log(history));
