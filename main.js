051d7026363aeb1c8c7794819809429483926ad7
Rui Alves <up201606746@fe.up.pt>
2019-10-09 14:36:09 +0100

    Add express API boilerplate

--- main.js
+++ main.js
@@ -1,36 +1,9 @@
 const fide_ratings = require("./fide_ratings");
+const express = require("express");
+const app = express();
 
-if (process.argv.length < 3) {
-    console.error("Arguments missing");
-    process.exit(1);
-}
+const port = process.env.PORT || 3000;
 
-fide_ratings.getPlayerElo(process.argv[2])
-    .then((elo) => {
-        console.log("----- Player ELO -----");
-        console.log(elo);
-    });
-
-fide_ratings.getPlayerHistory(process.argv[2])
-    .then((history) => {
-        console.log("----- Player History -----");
-        console.log(history);
-    });
-
-fide_ratings.getPlayerFullInfo(process.argv[2])
-    .then((info) => {
-        console.log("----- Player Info -----");
-        console.log(info);
-    });
-
-fide_ratings.getPlayerRank(process.argv[2])
-    .then((rank) => {
-        console.log("----- Player Rank -----");
-        console.log(rank);
-    });
-
-fide_ratings.getPlayerPersonalData(process.argv[2])
-    .then((data) => {
-        console.log("----- Player Personal Data -----");
-        console.log(data);
-    });
+app.listen(port, () =>
+    console.log(`Started listening on ${port} . . .`),
+);
