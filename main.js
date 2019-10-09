4a7261d41c7716372aa09d0f7f6672743ff4394d
Rui Alves <up201606746@fe.up.pt>
2019-10-09 17:09:38 +0100

    Add error catching to player endpoints

--- main.js
+++ main.js
@@ -38,41 +38,63 @@ app.get("/player/:fide_num/*", (req, res, next) => {
 app.get("/player/:fide_num/info", (req, res) => {
     const { fide_num } = req.params;
 
-    fide_ratings.getPlayerFullInfo(fide_num).then((data) => {
-        res.json(data);
-    });
+    fide_ratings.getPlayerFullInfo(fide_num)
+        .then((data) => res.json(data))
+        .catch(() => {
+            res.status(500).json(utils.buildErrorResponse(
+                "Failed to fetch player information",
+            ));
+        });
 });
 
 app.get("/player/:fide_num/personal-data", (req, res) => {
     const { fide_num } = req.params;
 
-    fide_ratings.getPlayerPersonalData(fide_num).then((data) => {
-        res.json(data);
-    });
+    fide_ratings.getPlayerPersonalData(fide_num)
+        .then((data) => res.json(data))
+        .catch(() => {
+            res.status(500).json(utils.buildErrorResponse(
+                "Failed to fetch player information",
+            ));
+        });
 });
 
 app.get("/player/:fide_num/rank", (req, res) => {
     const { fide_num } = req.params;
 
-    fide_ratings.getPlayerRank(fide_num).then((data) => {
-        res.json(data);
-    });
+    fide_ratings.getPlayerRank(fide_num)
+        .then((data) => res.json(data))
+        .catch(() => {
+            res.status(500).json(utils.buildErrorResponse(
+                "Failed to fetch player information",
+            ));
+        });
 });
 
 app.get("/player/:fide_num/elo", (req, res) => {
     const { fide_num } = req.params;
 
-    fide_ratings.getPlayerElo(fide_num).then((data) => {
-        res.json(data);
-    });
+    fide_ratings.getPlayerElo(fide_num)
+        .then((data) => res.json(data))
+        .catch(() => {
+            res.status(500).json(utils.buildErrorResponse(
+                "Failed to fetch player information",
+            ));
+        });
 });
 
 app.get("/player/:fide_num/history/", (req, res) => {
     const { fide_num } = req.params;
 
-    fide_ratings.getPlayerHistory(fide_num).then((data) => {
-        res.json(data);
-    });
+    fide_ratings.getPlayerHistory(fide_num)
+        .then((data) => {
+            res.json(data);
+        })
+        .catch(() => {
+            res.status(500).json(utils.buildErrorResponse(
+                "Failed to fetch player information",
+            ));
+        });
 });
 
 app.get("*", (req, res) => res.status(404).send(""));
