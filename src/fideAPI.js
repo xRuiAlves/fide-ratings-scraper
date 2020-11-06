c27d50d4758a4e56d120ff016cb435411e01a427
Rui Alves <up201606746@fe.up.pt>
2020-11-06 20:39:00 +0000

    Add ssl issues monkey patch

--- src/fideAPI.js
+++ src/fideAPI.js
@@ -1,4 +1,5 @@
 const axios = require("axios");
+const https = require("https");
 
 const FIDE_RATINGS_URL = "https://ratings.fide.com/profile";
 
@@ -9,7 +10,14 @@ const FIDE_RATINGS_URL = "https://ratings.fide.com/profile";
  * @returns {Object} Cheerio query object
  */
 const fetchProfilePage = (fide_num) => (
-    axios.get(`${FIDE_RATINGS_URL}/${fide_num}`)
+    axios.get(
+        `${FIDE_RATINGS_URL}/${fide_num}`,
+        {
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false,
+            }),
+        },
+    )
 );
 
 /**
@@ -19,7 +27,14 @@ const fetchProfilePage = (fide_num) => (
  * @returns {Object} Cheerio query object
  */
 const fetchHistoryPage = (fide_num) => (
-    axios.get(`${FIDE_RATINGS_URL}/${fide_num}/chart`)
+    axios.get(
+        `${FIDE_RATINGS_URL}/${fide_num}/chart`,
+        {
+            httpsAgent: new https.Agent({
+                rejectUnauthorized: false,
+            }),
+        },
+    )
 );
 
 module.exports = {
